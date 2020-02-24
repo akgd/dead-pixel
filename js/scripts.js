f1dc78cf1029aa1dc7eb8c361896b1bb735886a7
Andrea <andrea@brick.local>
2020-02-24 19:59:18 +0000

    Browser support

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,26 @@
-if (!document.fullscreenEnabled) {
-   alert('Your browser is not supported.')
-} else {
+console.log(document)
+if (document.fullscreenEnabled) {
     handleFullscreen();
+} else if (document.mozFullscreenEnabled) {
+    handleFullscreen('moz');
+
+} else if (document.webkitFullscreenEnabled) {
+    handleFullscreen('webkit');
+} else {
+    alert('Your browser is not supported.')
 }
 
-function handleFullscreen() {
+function handleFullscreen(prefix) {
     const fullscreenEls = document.querySelectorAll('.trigger');
-fullscreenEls.forEach(function (el) {
-    el.addEventListener('click', function() {
-        el.requestFullscreen();
+    fullscreenEls.forEach(function (el) {
+        el.addEventListener('click', function () {
+            if (!prefix) {
+                el.requestFullscreen();
+            } else if (prefix === 'webkit') {
+                el.webkitRequestFullscreen();
+            } else if (prefix === 'webkit') {
+                el.mozRequestFullscreen();
+            }
+        })
     })
-})
 }
